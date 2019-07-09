788f3721f3f5e8a2a4014b6686bc32fd463ac80b
Huidong Lin <oidiotlin@gmail.com>
2019-07-09 18:56:05 +0800

    采用 Tensorflow Mobile，弃用 Tensorflow Lite

--- app/src/main/java/com/facedetector/FaceDetectorActivity.java
+++ app/src/main/java/com/facedetector/FaceDetectorActivity.java
@@ -161,11 +161,7 @@ public class FaceDetectorActivity extends AppCompatActivity {
         }
 
         // 载入 DeepLab
-        try {
-            deeplab = new DeepLab(this, 2);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        deeplab = new DeepLab(getAssets());
     }
 
 
@@ -763,9 +759,7 @@ public class FaceDetectorActivity extends AppCompatActivity {
         @Override
         protected Boolean[][] doInBackground(byte[]... bytes) {
             Log.v(TAG, "call DeepLab function");
-            deeplab.setImageData(bytes[0]);
-            deeplab.runInference();
-            return deeplab.getTVSegment();
+            return deeplab.getTVSegment(bytes[0]);
         }
 
         @Override
